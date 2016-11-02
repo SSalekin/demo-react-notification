b1ea0226de4ce9acc14edcc110f42c2a35fd0dae
SSalekin <sjsalekin@gmail.com>
2016-11-02 20:17:30 +0600

    fix

--- components/Hello.js
+++ components/Hello.js
@@ -1,58 +1,66 @@
 import React, { PropTypes } from 'react';
-import { Button, Collapse, Form, FormGroup, FormControl, Col, ControlLabel, Checkbox } from 'react-bootstrap';
+import ReactDOM from 'react-dom';
+import { Button, Collapse, Form, FormGroup, FormControl, Col, ControlLabel, Checkbox, ButtonToolbar, Overlay, Popover } from 'react-bootstrap';
 
-class Hello extends React.Component {
-  constructor(...args) {
-    super(...args);
-    this.state = {};
-  }
+import CSSTransitionGroup from 'react-addons-css-transition-group';
+
+const CustomPopover = React.createClass({
+  render() {
+    return (
+      <CSSTransitionGroup
+        transitionName="slide"
+        transitionAppear={true}
+        transitionAppearTimeout={500}
+        transitionEnterTimeout={100}
+        transitionLeaveTimeout={300}
+      >
+        <div
+          style={{
+            position: 'absolute',
+            width: 200,
+            top: 0,
+            right: '50%',
+            backgroundColor: 'rgb(173, 228, 152',
+            boxShadow: '0 5px 10px rgba(0, 0, 0, 0.2)',
+            border: '1px solid #CCC',
+            borderRadius: 3,
+            padding: 10,
+          }}
+        >
+          <strong>Holy guacamole!</strong> Check this info.
+        </div>
+      </CSSTransitionGroup>
+    );
+  },
+});
+
+const Hello = React.createClass({
+  getInitialState() {
+    return { show: false };
+  },
+
+  toggle() {
+    this.setState({ show: !this.state.show });
+  },
 
   render() {
     return (
-      <div className="container">
-        <Button onClick={ ()=> this.setState({ open: !this.state.open })}>
-          Toggle
+      <div>
+        <Button ref="target" onClick={this.toggle}>
+          Notify
         </Button>
-        <Collapse in={this.state.open}>
-          <div className="well">
-            <Form horizontal>
-              <FormGroup controlId="formHorizontalEmail">
-                <Col componentClass={ControlLabel} sm={2}>
-                  Email
-                </Col>
-                <Col sm={10}>
-                  <FormControl type="email" placeholder="Email" />
-                </Col>
-              </FormGroup>
 
-              <FormGroup controlId="formHorizontalPassword">
-                <Col componentClass={ControlLabel} sm={2}>
-                  Password
-                </Col>
-                <Col sm={10}>
-                  <FormControl type="password" placeholder="Password" />
-                </Col>
-              </FormGroup>
-
-              <FormGroup>
-                <Col smOffset={2} sm={10}>
-                  <Checkbox>Remember me</Checkbox>
-                </Col>
-              </FormGroup>
-
-              <FormGroup>
-                <Col smOffset={2} sm={10}>
-                  <Button type="submit">
-                    Sign in
-                  </Button>
-                </Col>
-              </FormGroup>
-            </Form>
-          </div>
-        </Collapse>
+        <Overlay
+          show={this.state.show}
+          onHide={() => this.setState({ show: false })}
+          container={this}
+          target={() => ReactDOM.findDOMNode(this.refs.target)}
+        >
+          <CustomPopover />
+        </Overlay>
       </div>
     );
-  }
-}
+  },
+});
 
 export default Hello
